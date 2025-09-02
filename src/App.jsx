b4f1500ab22fdf90432818fe4374b2ddb3f5db6d
Kim Valentin <co.unicorno@mail.ru>
2025-09-02 17:18:01 +0400

    feat(ui): implement hero section layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import gsap from "gsap"
 import { ScrollTrigger, SplitText } from "gsap/all"
 import NavBar from "./components/NavBar"
+import Hero from "./components/Hero"
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 const App = () => {
   return (
     <main>
       <NavBar/>
+      <Hero/>
     </main>
   )
 }
