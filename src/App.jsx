e168a332002d39ba0e92fbd8054a694057b038cf
Kim Valentin <co.unicorno@mail.ru>
2025-09-03 17:41:41 +0400

    feat: Implement Cocktails section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import gsap from "gsap"
 import { ScrollTrigger, SplitText } from "gsap/all"
 import NavBar from "./components/NavBar"
 import Hero from "./components/Hero"
+import Cocktails from "./components/Cocktails"
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <main>
       <NavBar/>
       <Hero/>
+      <Cocktails/>
     </main>
   )
 }
